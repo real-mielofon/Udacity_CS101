d4761312945e2ad574a5091fa68927ddb27a8913
Alexey Ponomarev <mielofon@gmail.com>
2012-04-05 13:08:52 +0400

    change

--- Unit6/Homework/3.py
+++ Unit6/Homework/3.py
@@ -28,24 +28,19 @@ def hexes_to_udaciousness(n, spread, target):
 
 
 #0 more needed, since n already exceeds target
-print hexes_to_udaciousness(100000, 2, 36230) 
-#>>> 0
+print hexes_to_udaciousness(100000, 2, 36230), ' = 0'
 
 #after 1 hexamester, there will be 50000 + (50000 * 2) Udacians
-print hexes_to_udaciousness(50000, 2, 150000) 
-#>>> 1 
+print hexes_to_udaciousness(50000, 2, 150000), ' = 1' 
 
 #need to match or exceed the target
-print hexes_to_udaciousness(50000, 2, 150001)
-#>>> 2 
+print hexes_to_udaciousness(50000, 2, 150001), ' = 2' 
 
 #only 12 hexamesters (2 years) to world domination!
-print hexes_to_udaciousness(20000, 2, 7 * 10 ** 9) 
-#>>> 12 
+print hexes_to_udaciousness(20000, 2, 7 * 10 ** 9), ' = 12' 
 
 #more friends means faster world domination!
-print hexes_to_udaciousness(15000, 3, 7 * 10 ** 9)
-#>>> 10 
+print hexes_to_udaciousness(15000, 3, 7 * 10 ** 9), ' = 10' 
 
 
 
